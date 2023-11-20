3d5120b91c216eaaccb03f0e4db6a471f417b83f
Mahmoud_Elshiha <mahmoudelshiha@gmail.com>
2023-11-21 00:27:17 +0200

    getting Login pages ready

--- templates/header.php
+++ templates/header.php
@@ -4,6 +4,8 @@ function head($currentPage)
 {
 
 ?>
+
+
   <section id="header">
     <a href="#"><img src="./images/logo.svg" class="logo" alt="" /></a>
     <div>
@@ -14,13 +16,21 @@ function head($currentPage)
         <li><a href="about.php" <?php echo ($currentPage == "about") ? "class = 'active' " : " "; ?>> About </a></li>
         <li><a href="contact.php" <?php echo ($currentPage == "contact") ? "class = 'active' " : " "; ?>> Contact </a></li>
 
-        <li>
-          <a href="cart.php"><i class="fa-solid fa-cart-shopping"></i></a>
-        </li>
+        <li> <a href="cart.php"><i class="fa-solid fa-cart-shopping"></i></a></li>
+        <li> <a><i class="fa-solid fa-circle-user" id="user-circle"></i></a> </li>
       </ul>
     </div>
-  </section>
+    <div class="profile">
 
+      <p> Please Login Or Register First ...</p> <br>
+      <div class="row">
+        <a href="../user_login.php"><button class="normal">Login</button></a>
+        <a href="../user_register.php"><button class="normal">Register</button></a>
+      </div>
+
+    </div>
+  </section>
+  <script src="../scripts/script.js"></script>
 <?php
 }
 ?>
