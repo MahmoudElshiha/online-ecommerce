4cf8021d76f2fb8fc7d1482971961fe481482c65
abdsalama <abdsalama2020@gmail.com>
2023-12-02 20:54:59 +0200

    about done &Some modifications to contact

--- contact.php
+++ contact.php
@@ -20,24 +20,10 @@
     
     </head>
 <body>
-    <section id="header">
-        <a href="#"><img src="images/loge.png" class="logo" alt=""></a>
-        <div>
-            <ul id="navbar">
-                <li><a href="index.html">Home</a></li>
-                <li><a href="shop.html">Shop</a></li>
-                <li><a href="blog.html">Blog</a></li>
-                <li><a href="about.html">About</a></li>
-                <li><a class="active" href="contact.html">contact</a></li>
-                <li id="lg-bag"><a href="cart.html"><i class="far fa-shopping-bag"></i></a></li>
-                <a href="#" id="close"><i class="far fa-times"></i></a>
-            </ul>
-        </div>
-        <div id="mobile">
-            <a href="cart.html"><i class="far fa-shopping-bag"></i></a>
-            <i id="bar" class="fas fa-outdent"></i>
-        </div>
-    </section>
+<?php
+  include "./templates/header.php";
+  head("contact");
+  ?>
     <section id="page-heder" class="about-header">
         <h2>#let's_talk</h2>
         <p>LEAVE A MASSAGE, We love to hear from you!</p>
@@ -120,51 +106,6 @@
         <button class="normal">Sign Up</button>
     </div>
 </section>
-<footer class="section-p1">
-    <div class="col">
-        <img class="logo" src="./images/logo.svg" alt="" />
-        <h4>Contact</h4>
-        <p><strong>Address: </strong>Damitta_New-Damitta</p>
-        <p><strong>Phone: </strong>+201015202628 / +201094282425</p>
-        <p><strong>Hours: </strong>10:00 -18:00, Sat - Thrus</p>
-    </div>
-
-    <div class="follow">
-        <h4>Follow us</h4>
-        <div>
-        <i class="fab fa-facebook"></i>
-        <i class="fab fa-twitter"></i>
-        <i class="fab fa-instagram"></i>
-        <i class="fab fa-youtube"></i>
-        </div>
-    </div>
-    <div class="col">
-        <h4>About</h4>
-        <a href="#">About Us</a>
-        <a href="#">Delivery Information</a>
-        <a href="#">Privacy Policy</a>
-        <a href="#">Terms & Conditions</a>
-        <a href="#">Contact Us</a>
-    </div>
-
-    <div class="col">
-        <h4>My Account</h4>
-        <a href="#">Sign In</a>
-        <a href="#">View Cart</a>
-        <a href="#">My Wishlist</a>
-        <a href="#">Track My Order</a>
-        <a href="#">Help</a>
-    </div>
-    <div class="col install">
-        <h4>Install App</h4>
-        <p>From App Store or Google Play</p>
-        <div class="row">
-        <img src="./images/pay/app.jpg" alt="" />
-        <img src="./images/pay/play.jpg" alt="" />
-        </div>
-        <p>Secured Payment Getaways</p>
-        <img src="./images/pay/pay.png" alt="" />
-    </div>
-</footer>
+<?php include "./templates/footer.php" ?>
 </body>
 </html>
