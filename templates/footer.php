beb29d6de63c362a7a48f76d311700f6c975d945
Mahmoud_Elshiha <mahmoudelshiha@gmail.com>
2023-12-09 16:02:59 +0200

    hide the app from footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,46 +1,46 @@
 <footer class="section-p1">
-    <div class="col">
-      <img class="logo" src="./images/logo.svg" alt="" />
-      <h4>Contact</h4>
-      <p><strong>Address: </strong>Damitta_New-Damitta</p>
-      <p><strong>Phone: </strong>+201015202628 / +201094282425</p>
-      <p><strong>Hours: </strong>10:00 -18:00, Sat - Thrus</p>
-    </div>
+  <div class="col">
+    <img class="logo" src="./images/logo.svg" alt="" />
+    <h4>Contact</h4>
+    <p><strong>Address: </strong>Damitta_New-Damitta</p>
+    <p><strong>Phone: </strong>+201015202628 / +201094282425</p>
+    <p><strong>Hours: </strong>10:00 -18:00, Sat - Thrus</p>
+  </div>
 
-    <div class="follow">
-      <h4>Follow us</h4>
-      <div>
-        <i class="fab fa-facebook"></i>
-        <i class="fab fa-twitter"></i>
-        <i class="fab fa-instagram"></i>
-        <i class="fab fa-youtube"></i>
-      </div>
+  <div class="follow">
+    <h4>Follow us</h4>
+    <div>
+      <i class="fab fa-facebook"></i>
+      <i class="fab fa-twitter"></i>
+      <i class="fab fa-instagram"></i>
+      <i class="fab fa-youtube"></i>
     </div>
-    <div class="col">
-      <h4>About</h4>
-      <a href="#">About Us</a>
-      <a href="#">Delivery Information</a>
-      <a href="#">Privacy Policy</a>
-      <a href="#">Terms & Conditions</a>
-      <a href="#">Contact Us</a>
-    </div>
+  </div>
+  <div class="col">
+    <h4>About</h4>
+    <a href="#">About Us</a>
+    <a href="#">Delivery Information</a>
+    <a href="#">Privacy Policy</a>
+    <a href="#">Terms & Conditions</a>
+    <a href="#">Contact Us</a>
+  </div>
 
-    <div class="col">
-      <h4>My Account</h4>
-      <a href="#">Sign In</a>
-      <a href="#">View Cart</a>
-      <a href="#">My Wishlist</a>
-      <a href="#">Track My Order</a>
-      <a href="#">Help</a>
+  <div class="col">
+    <h4>My Account</h4>
+    <a href="#">Sign In</a>
+    <a href="#">View Cart</a>
+    <a href="#">My Wishlist</a>
+    <a href="#">Track My Order</a>
+    <a href="#">Help</a>
+  </div>
+  <!-- <div class="col install">
+    <h4>Install App</h4>
+    <p>From App Store or Google Play</p>
+    <div class="row">
+      <img src="./images/pay/app.jpg" alt="" />
+      <img src="./images/pay/play.jpg" alt="" />
     </div>
-    <div class="col install">
-      <h4>Install App</h4>
-      <p>From App Store or Google Play</p>
-      <div class="row">
-        <img src="./images/pay/app.jpg" alt="" />
-        <img src="./images/pay/play.jpg" alt="" />
-      </div>
-      <p>Secured Payment Getaways</p>
-      <img src="./images/pay/pay.png" alt="" />
-    </div>
-  </footer>
+    <p>Secured Payment Getaways</p>
+    <img src="./images/pay/pay.png" alt="" />
+  </div> -->
+</footer>
